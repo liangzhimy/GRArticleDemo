becd80de34eac9017ba67ef1654cdc9c31060dfe
liangzhimy <liangzhimy@gmail.com>
2017-06-29 16:29:41 +0800

    Initial Commit

---
+++ GRArticleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GRArticleDemo
+//
+//  Created by liangzhimy on 2017/6/29.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GRArticleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GRArticleDemo
+//
+//  Created by liangzhimy on 2017/6/29.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
